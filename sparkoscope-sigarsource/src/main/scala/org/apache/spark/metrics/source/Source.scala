ba4feab89ba3de917d748ad3fa550e696429fe99
yiannisg <yiannisg@ie.ibm.com>
2016-11-02 16:34:12 +0000

    imported

---
+++ sparkoscope-sigarsource/src/main/scala/org/apache/spark/metrics/source/Source.scala
@@ -0,0 +1,8 @@
+package org.apache.spark.metrics.source
+
+import com.codahale.metrics.MetricRegistry
+
+private[spark] trait Source {
+  def sourceName: String
+  def metricRegistry: MetricRegistry
+}
